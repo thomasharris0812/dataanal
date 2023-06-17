c891fe0ad032b0b392bb6af63bd844ca2f584ec7
thomasharris0812 <136863940+thomasharris0812@users.noreply.github.com>
2023-06-17 17:46:42 -0500

    Update musiclibrary.sql

--- musiclibrary.sql
+++ musiclibrary.sql
@@ -33,3 +33,12 @@ CREATE TABLE client (
   branch_id INT,
   FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL
 );
+
+CREATE TABLE works_with (
+  emp_id INT,
+  client_id INT,
+  total_sales INT,
+  PRIMARY KEY(emp_id, client_id),
+  FOREIGN KEY(emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
+  FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
+);
