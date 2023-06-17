f76994aaa50289ac0ad9a6e4336de30552a5c670
thomasharris0812 <136863940+thomasharris0812@users.noreply.github.com>
2023-06-17 17:35:38 -0500

    Update musiclibrary.sql

--- musiclibrary.sql
+++ musiclibrary.sql
@@ -2,9 +2,17 @@ CREATE TABLE employee (
   emp_id INT PRIMARY KEY,
   first_name VARCHAR(40) NOT NULL,
   last_name VARCHAR(40) NOT NULL,
-  birthday DATE,
-  sex VARCHAR(1),
-  salary INT,
-  super_ID INT,
-  branch_ID INT
+  birthday DATE NOT NULL,
+  sex VARCHAR(1) NOT NULL,
+  salary INT NOT NULL,
+  super_id INT,
+  branch_id INT NOT NULL
+);
+
+CREATE TABLE branch (
+  branch_id INT PRIMARY KEY,
+  branch_name VARCHAR(40),
+  mngr_id INT,
+  mngr_start_date DATE,
+  FOREIGN KEY(mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL
 );
