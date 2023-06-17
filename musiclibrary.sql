bb443f570c21c51f36a90c6ce676ce27e9bc65b5
thomasharris0812 <136863940+thomasharris0812@users.noreply.github.com>
2023-06-17 18:07:20 -0500

    Update musiclibrary.sql

--- musiclibrary.sql
+++ musiclibrary.sql
@@ -50,3 +50,13 @@ CREATE TABLE branch_supplier (
   PRIMARY KEY(branch_id, supplier_name),
   FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
 );
+
+INSERT INTO employee VALUES(100, 'David', 'Wallace', '1967-11-17', 'M', 250000, NULL, NULL);
+
+INSERT INTO branch VALUES(1, 'Corporate', 100, '2006-02-09');
+
+UPDATE employee
+SET branch_id = 1
+WHERE emp_id = 100;
+
+INSERT INTO employee VALUES(101, 'Jan', 'Levinson', '1961-05-11', 'F', 100000, 100, 1);
