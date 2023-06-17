a143c88b402edb6f84bdc7abe8a541d1d72d76da
thomasharris0812 <136863940+thomasharris0812@users.noreply.github.com>
2023-06-17 17:38:42 -0500

    Update musiclibrary.sql

--- musiclibrary.sql
+++ musiclibrary.sql
@@ -16,3 +16,13 @@ CREATE TABLE branch (
   mngr_start_date DATE,
   FOREIGN KEY(mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL
 );
+
+ALTER TABLE employee
+ADD FOREIGN KEY(branch_id)
+REFERENCES branch(branch_id)
+ON DELETE SET NULL;
+
+ALTER TABLE employee
+ADD FOREIGN KEY(super_id)
+REFERENCES employee(emp_id)
+ON DELETE SET NULL;
